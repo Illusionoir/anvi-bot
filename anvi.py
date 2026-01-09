66f02b0fd2dfe27efa83cd7ed254aaa4dd2c4374
Illusion <rohanthakur2684@gmail.com>
2026-01-09 11:05:59 +0530

    Update anvi.py

--- anvi.py
+++ anvi.py
@@ -33,7 +33,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Anvi is alive!"
+    return redirect("https://illusionoir.github.io/anvi", code=302)
 
 def run_web():
     app.run(host="0.0.0.0", port=8080)
